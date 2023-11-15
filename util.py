1c7f48559b8129275e94dc32359183dc42c7e623
sucharush <siyuancheng9725@gmail.com>
2023-11-15 13:46:54 +0100

    updated data preprocessing

---
+++ util.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as up
+
+def split_matched_data(data):
+    """split data from the two websites"""
+    data_rb = data.filter(like='rb', axis=1)ß
+    data_ba = data.filter(like='ba', axis=1)
+    data_rb.columns = data_rb.iloc[0]
+    data_rb = data_rb.iloc[1:].reset_index(drop=True)
+    data_ba.columns = data_ba.iloc[0]
+    data_ba = data_ba.iloc[1:].reset_index(drop=True)
+    return data_rb, data_ba
