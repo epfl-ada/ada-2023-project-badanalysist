a9705764f2f137098274a66cd1b9684adc25a021
YXDXY <xinyiding00@gmail.com>
2023-12-05 15:53:26 +0100

    update web

--- script/scroll.js
+++ script/scroll.js
@@ -1,20 +1,12 @@
-document.querySelector('.content').addEventListener('wheel', function(e) {
-    e.preventDefault();
-    var scrollPosition = document.querySelector('.content').scrollLeft;
-    var sections = document.querySelectorAll('.content-section');
-    var boxes = document.querySelectorAll('.topic .box');
-    this.scrollLeft += e.deltaY;
+window.addEventListener('wheel', function() {
+    var topic = document.querySelector('.topic');
+    var topicOffset = topic.getBoundingClientRect().top; 
 
-    boxes.forEach(function(box) {
-        box.classList.remove('selected');
-    });
-
-    sections.forEach(function(section, index) {
-        var sectionStart = section.offsetLeft;
-        var sectionEnd = sectionStart + section.offsetWidth;
-
-        if (scrollPosition >= sectionStart && scrollPosition < sectionEnd) {
-            boxes[index].classList.add('selected');
-        }
-    });
+    if (topicOffset <= 0) {
+        topic.style.position = 'fixed';
+        topic.style.top = '0'; 
+    } else {
+        topic.style.position = 'absolute'; 
+        topic.style.top = null; 
+    }
 });
