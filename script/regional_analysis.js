04d5324ed055d58ce572f90ee6b039944bc55e1d
Ivonne320 <ivonne.don.epfl@gmail.com>
2023-12-14 11:03:42 +0100

    update regional_analysis js

--- script/regional_analysis.js
+++ script/regional_analysis.js
@@ -1,41 +1,43 @@
 fetch('viz_data/regional_analysis.json')
     .then(response => response.json())
     .then(data => {
-        // Process the data
-        prepareHoverText(data);
+        prepareHoverTextAndPlot(data);
     })
     .catch(error => {
         console.error('Error fetching data: ', error);
     });
 
-function prepareHoverText(data) {
-    data.forEach(region => {
+function prepareHoverTextAndPlot(regions) {
+    regions.forEach(region => {
         region.hover_text = `Region: ${region.region}<br>` +
                             `Rank: ${region.rank}<br>` +
                             `Popular Beer: ${region.popular_beers} (Count: ${region.counts_pop_beers.toFixed(2)}, Rating: ${region.rating_pop_beers.toFixed(2)})<br>` +
                             `Popular Style: ${region.popular_styles} (Count: ${region.counts_pop_styles.toFixed(2)}, Rating: ${region.rating_pop_styles.toFixed(2)})`;
     });
+
+    createMap(regions);
 }
 
-// Plotly map
-var data = [{
-    type: 'scattergeo',
-    mode: 'markers',
-    locations: regions.map(region => region.region), 
-    text: regions.map(region => region.hover_text),
-    hoverinfo: 'text',
-    marker: {
-        size: 10
-    }
-}];
-
-var layout = {
-    title: 'Beer Preferences by Region',
-    geo: {
-        projection: {
-            type: 'natural earth'
+function createMap(regions) {
+    var data = [{
+        type: 'scattergeo',
+        mode: 'markers',
+        locations: regions.map(region => region.region), 
+        text: regions.map(region => region.hover_text),
+        hoverinfo: 'text',
+        marker: {
+            size: 10
         }
-    }
-};
+    }];
 
-Plotly.newPlot('regional_analysis', data, layout);
+    var layout = {
+        title: 'Beer Preferences by Region',
+        geo: {
+            projection: {
+                type: 'natural earth'
+            }
+        }
+    };
+
+    Plotly.newPlot('regional_analysis', data, layout);
+}
