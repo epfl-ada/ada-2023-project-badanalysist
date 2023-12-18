326afe9ada9c19f1186917e955011341edf46315
sucharush <siyuancheng9725@gmail.com>
2023-12-18 19:49:56 +0100

    update script

--- script/feature-importance.js
+++ script/feature-importance.js
@@ -1,37 +1,3 @@
-// // Load the JSON data
-// fetch('viz_data/feature_importance.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     // Process the data and create the chart
-//     createFeatureImportanceChart(data);
-//   })
-//   .catch(error => console.error('Error loading the feature importance data:', error));
-
-// // Function to create the Plotly chart
-// function createFeatureImportanceChart(data) {
-//   var traces = [];
-  
-//   // Create a trace for each cluster
-//   data.forEach((cluster, index) => {
-//     var trace = {
-//       x: cluster.features,
-//       y: cluster.importances,
-//       type: 'bar',
-//       name: `Cluster ${cluster.cluster}`
-//     };
-//     traces.push(trace);
-//   });
-
-//   var layout = {
-//     // Define the layout
-//     title: 'Feature Importances by Cluster',
-//     barmode: 'group'
-//   };
-
-//   // Create the Plotly chart
-//   Plotly.newPlot('feature_importance', traces, layout);
-// }
-
 fetch('viz_data/feature_importance.json')
   .then(res => res.json())
   .then(radarChartData => {
