2e28d931825ae8115dbc46d6ced71a58b9dec97f
CZX <145619214+CZX4810@users.noreply.github.com>
2023-12-22 12:16:10 +0100

    Add files via upload
    update import

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import requests
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -6,14 +8,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 from fuzzywuzzy import process
-import requests
 from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag
 from nltk.corpus import stopwords
 from collections import Counter
+from langdetect import detect
+from transformers import pipeline
 
  
 def split_matched_data(data):
